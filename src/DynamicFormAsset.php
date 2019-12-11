2b2e0f39d89e474c5fed0e86cb11bb44007c5f15
Vitaly Walkboy <walkboy@mail.ru>
2019-12-11 23:42:30 +0300

    Removed minified assets setup

--- src/DynamicFormAsset.php
+++ src/DynamicFormAsset.php
@@ -22,42 +22,9 @@ class DynamicFormAsset extends \yii\web\AssetBundle
         'yii\widgets\ActiveFormAsset'
     ];
 
-    /**
-     * Set up CSS and JS asset arrays based on the base-file names
-     * @param string $type whether 'css' or 'js'
-     * @param array $files the list of 'css' or 'js' basefile names
-     */
-    protected function setupAssets($type, $files = [])
-    {
-        $srcFiles = [];
-        $minFiles = [];
-        foreach ($files as $file) {
-            $srcFiles[] = "{$file}.{$type}";
-            $minFiles[] = "{$file}.min.{$type}";
-        }
-        if (empty($this->$type)) {
-            $this->$type = YII_DEBUG ? $srcFiles : $minFiles;
-        }
-    }
+    public $sourcePath = __DIR__.'/assets';
 
-    /**
-     * @inheritdoc
-     */
-    public function init()
-    {
-        $this->setSourcePath(__DIR__ . '/assets');
-        $this->setupAssets('js', ['yii2-dynamic-form']);
-        parent::init();
-    }
-
-    /**
-     * Sets the source path if empty
-     * @param string $path the path to be set
-     */
-    protected function setSourcePath($path)
-    {
-        if (empty($this->sourcePath)) {
-            $this->sourcePath = $path;
-        }
-    }
+    public $js = [
+        'yii2-dynamic-form.js',
+    ];
 }
